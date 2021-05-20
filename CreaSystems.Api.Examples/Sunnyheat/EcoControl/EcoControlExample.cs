7c78a31bb22874f322c62af4f23c0ae94af1bc04
creasystems <49979593+creasystems@users.noreply.github.com>
2021-05-20 17:06:52 +0200

    new: Project from local files

---
+++ CreaSystems.Api.Examples/Sunnyheat/EcoControl/EcoControlExample.cs
@@ -0,0 +1,59 @@
+﻿//-------------------------------------------------------------------------------------------------------
+// <copyright file="EcoControlExample.cs" company="CREA SYSTEMS Electronic GmbH">
+// Copyright (c) CREA SYSTEMS Electronic GmbH. All rights reserved.
+//
+// CREA SYSTEMS Electronic GmbH licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+//
+// <author>CREA SYSTEMS Electronic GmbH</author>
+//-------------------------------------------------------------------------------------------------------
+
+namespace CreaSystems.Api.Examples.Sunnyheat.EcoControl
+{
+    #region Namespaces
+
+    using CreaSystems.Api.Sunnyheat.EcoControl;
+
+    #endregion Namespaces
+
+    /// <summary>
+    /// Example program to smart control the SUNNYHEAT infrared heaters.
+    /// </summary>
+    internal class EcoControlExample
+    {
+        #region Variables
+
+        /// <summary>
+        /// The heater list
+        /// </summary>
+        private SunnyheatInfraredHeaterList heaterList;
+
+        #endregion Variables
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EcoControlExample"/> class.
+        /// </summary>
+        internal EcoControlExample()
+        {
+            InitializeHeaters();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the heaters.
+        /// </summary>
+        private void InitializeHeaters()
+        {
+            heaterList = new SunnyheatInfraredHeaterList();
+            heaterList.AddHeater(new SunnyheatInfraredHeater("192.168.20.127", "Living room"));
+        }
+
+        #endregion Methods
+    }
+}
---
+++ CreaSystems.Api.Examples/Sunnyheat/EcoControl/Program.cs
@@ -0,0 +1,32 @@
+﻿//-------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="CREA SYSTEMS Electronic GmbH">
+// Copyright (c) CREA SYSTEMS Electronic GmbH. All rights reserved.
+//
+// CREA SYSTEMS Electronic GmbH licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// </copyright>
+//
+// <author>CREA SYSTEMS Electronic GmbH</author>
+//-------------------------------------------------------------------------------------------------------
+
+namespace CreaSystems.Api.Examples.Sunnyheat.EcoControl
+{
+    /// <summary>
+    /// Entry point for the example program to smart control the SUNNYHEAT infrared heaters.
+    /// </summary>
+    public class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            new EcoControlExample();
+        }
+
+        #endregion Methods
+    }
+}
